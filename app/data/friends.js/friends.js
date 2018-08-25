eb10aecf2a6aeea893369b3d3e11244c9dc7edb0
Trina Raye <jonescatrina1@gmail.com>
2018-08-25 12:51:21 -0400

    Add everything from the .zip folder

---
+++ app/data/friends.js/friends.js
@@ -0,0 +1,61 @@
+// You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+
+var friendFinder = [
+    {
+
+  "name":"Mable Simmons",
+  "photo":"https://pbs.twimg.com/profile_images/1629827473/image_400x400.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+  ]
+},
+
+{
+    "name":"Jamie Foxx",
+    "photo":"https://steemitimages.com/DQmWMPHtL331STbymkmMc3Edm1WXU7AS9SFnnrxjhAyrkMX/Jamie%20Foxx.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+  },
+
+{
+
+  "name":"Michelle Obama",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/4/4b/Michelle_Obama_2013_official_portrait.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+]
+
+module.exports = friends;
+  
+
+
